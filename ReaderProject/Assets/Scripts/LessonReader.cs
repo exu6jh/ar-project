6416c8977952b187af177f7565e1af04b1c6bd54
rcheng15jhu <rcheng15@jhu.edu>
2022-12-07 20:55:38 -0500

    Newest LessonReader version

--- ReaderProject/Assets/Scripts/LessonReader.cs
+++ ReaderProject/Assets/Scripts/LessonReader.cs
@@ -1,120 +1,137 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEditor;
-using Object = UnityEngine.Object;
 
 public class LessonReader : MonoBehaviour
 {
-    public string[] commands;
+    public string filename;
+    private string[] commands;
     
     private Dictionary<string, bool> flags;
-    private Dictionary<string, Object> objects;
+    private Dictionary<string, GameObject> gameObjects;
+    private Dictionary<string, Matrix> matrices;
+
+    private string savePhrase;
     private int savePoint;
 
     // Start is called before the first frame update
     void Start()
     {
+        commands = System.IO.File.ReadAllLines(filename);
+
         flags = new Dictionary<string, bool>();
+        gameObjects = new Dictionary<string, GameObject>();
+        matrices = new Dictionary<string, Matrix>();
         Execute(0);
     }
 
     void Execute(int index) {
         // Handle out of bounds exceptions.
         if(index >= commands.Length) {
+            savePoint = -1;
             Debug.Log("Script execution terminated at end of file.");
             return;
         }
 
         savePoint = index;
-        string[] scriptWords = commands[index].Split(" ", StringSplitOptions.RemoveEmptyEntries/* | StringSplitOptions.TrimEntries */);
 
-        // DEBUG: print out all parsed words in sentence.
-        for(int i = 0; i < scriptWords.Length; i++) {
-            Debug.Log(scriptWords[i]);
-        }
-
-        if(scriptWords[0].Equals("CREATE-OBJECT")) {
+        if(Regex.Match(commands[index], "^CREATE-OBJECT \"[\\w\\-. ]+\"( \"[\\w\\-. ]+\")?$").Success) {
             Debug.Log("Creating.");
-            
-            string[] names = commands[index].Split("\"", StringSplitOptions.RemoveEmptyEntries/* | StringSplitOptions.TrimEntries */);
-            for(int i = 0; i < names.Length; i++) {
-                Debug.Log(names[i]);
-            }
+            string[] names = commands[index].Split("\"");
 
-            string[] matchingAssets = AssetDatabase.FindAssets(names[1]);
-            string dataPath = AssetDatabase.GUIDToAssetPath(matchingAssets[0]);
-            GameObject obj = (GameObject)AssetDatabase.LoadAssetAtPath(dataPath, typeof(GameObject));
-            GameObject newObj = Instantiate(obj);
+            try {
+                string[] matchingAssets = AssetDatabase.FindAssets(names[1]);
+                string dataPath = AssetDatabase.GUIDToAssetPath(matchingAssets[0]);
+                GameObject obj = (GameObject)AssetDatabase.LoadAssetAtPath(dataPath, typeof(GameObject));
+                GameObject newObj = Instantiate(obj);
 
-            int j = 1;
-            while(j < names.Length) {
-                if(!string.IsNullOrWhiteSpace(names[j])) {
-                    newObj.name = names[j];
-                    break;
+                if(names.Length > 2) {
+                    newObj.name = names[3];
+                    gameObjects[names[3]] = newObj;
+                } else {
+                    gameObjects[names[1]] = newObj;
                 }
-                j++;
+            } catch {
+                Debug.Log(string.Format("Asset \"{0}\" not found.", names[1]));
             }
 
             Execute(index + 1);
-        } else if(scriptWords[0].Equals("DELETE-OBJECT")) {
+        } else if (Regex.Match(commands[index], "^CREATE-MATRIX \"[\\w\\- ]+\" \\[(([0-9]+(.[0-9]+)?)(,[0-9]+(.[0-9]+)?)*)(;([0-9]+(.[0-9]+)?)(,[0-9]+(.[0-9]+)?)*)*\\]$").Success) {
+            try {
+                string matrixElements = commands[index].Split(new char[] {'[', ']'})[1];
+                string[] rows = matrixElements.Split(';');
+                string[] referenceCols = rows[0].Split(",");
+                float[,] values = new float[rows.Length, referenceCols.Length];
+                for(int i = 0; i < rows.Length; i++) {
+                    string[] cols = rows[i].Split(",");
+                    if(cols.Length != referenceCols.Length) {
+                        Debug.Log("Length mismatch.");
+                        throw new System.ArithmeticException();
+                    }
+                    for(int j = 0; j < cols.Length; j++) {
+                        values[i,j] = float.Parse(cols[j]);
+                    }
+                }
+                string[] names = commands[index].Split("\"");
+                matrices[names[1]] = new Matrix(values);
+            } catch {
+                Debug.Log(string.Format("Invalid 3 x 3 matrix supplied in command line {0}", index));
+                Debug.Log("All such matrices must be formatted like [A, B, C; D, E, F; G, H, I]");
+            }
+
+            Execute(index + 1);
+        } else if(Regex.Match(commands[index], "^DELETE-OBJECT \"[\\w\\-. ]+\"$").Success) {
             Debug.Log("Deleting.");
-
-            string[] names = commands[index].Split("\"", StringSplitOptions.RemoveEmptyEntries/* | StringSplitOptions.TrimEntries */);
-            for(int i = 0; i < names.Length; i++) {
-                Debug.Log(names[i]);
-            }
-            GameObject obj = GameObject.Find(names[1]);
-            if(obj) {
-                Destroy(obj);
+            string[] names = commands[index].Split("\"");
+            if(gameObjects.ContainsKey(names[1])) {
+                Destroy(gameObjects[names[1]]);
+            } else {
+                Debug.Log(string.Format("No such object {0} detected.", names[1]));
             }
 
             Execute(index + 1);
-        } else if(scriptWords[0].Equals("WAIT")) {
+        } else if(Regex.Match(commands[index], "^WAIT [0-9]+(.[0-9]+)?$").Success) {
             Debug.Log("Waiting.");
             try {
+                string[] scriptWords = commands[index].Split(" ");
                 float waitTime = float.Parse(scriptWords[1]);
                 Debug.Log("Starting wait.");
                 IEnumerator waitCoroutine = Wait(waitTime, index + 1);
                 StartCoroutine(waitCoroutine);
             } catch {
-                Debug.Log(string.Format("Invalid wait time supplied in command line {0}.", index));
-                Debug.Log("All wait commands must be formatted like \"WAIT <float>\". For example, \"WAIT 24.1\".");
+                Debug.Log(string.Format("Problem parsing wait time in command line {0}.", index));
             }
-        } else if(scriptWords[0].Equals("WAIT-UNTIL")) {
+        } else if(Regex.Match(commands[index], "^WAIT-UNTIL \"[\\w\\- ]+\"$").Success) {
+            Debug.Log("WAIT-UNTIL");
+            string[] names = commands[index].Split("\"");
+            savePhrase = names[1];
             savePoint = index + 1;
-            Debug.Log("WAIT UNTIL");
-        } else if(scriptWords[0].Equals("GOTO")) {
+        } else if(Regex.Match(commands[index], "^GOTO [0-9]+( IF \"[\\w-]+\" ELSE [0-9]+)?$").Success) {
             Debug.Log("GOTO");
+            string[] scriptWords = commands[index].Split(" ");
             try {
-                int gotoIndex = int.Parse(scriptWords[1]);
-                if(scriptWords.Length >= 5) {
+                int truePath = int.Parse(scriptWords[1]);
+                if(scriptWords.Length > 2) {
+                    string[] names = commands[index].Split("\"");
                     try {
-                        bool value = flags[scriptWords[2]];
-                        int truePath = int.Parse(scriptWords[3]);
-                        int falsePath = int.Parse(scriptWords[4]);
+                        bool value = flags[names[1]];
+                        int falsePath = int.Parse(scriptWords[5]);
                         Execute(value ? truePath : falsePath);
                     } catch {
                         Debug.Log(string.Format("goto if path detected, but invalid command line choices provided in command line {0}", index));
                         Debug.Log("All goto if commands must be formatted like \"GOTO <int> <value> <path if true> <path if false>\"");
                     }
                 } else {
-                    Execute(gotoIndex);
+                    Execute(truePath);
                 }
             } catch {
                 Debug.Log(string.Format("Invalid goto line supplied in command line {0}", index));
                 Debug.Log("All goto commands must be formatted like \"GOTO <int>\". For example, \"GOTO 10\".");
             }
-        } else if (scriptWords[0].Equals("MATRIX")) {
-            try {
-                string[] terms = commands[index].Split("\"", StringSplitOptions.RemoveEmptyEntries/* | StringSplitOptions.TrimEntries */);
-                string[] matrixElements = terms[2].Split(new char[] {'[', ']'}, StringSplitOptions.RemoveEmptyEntries/* | StringSplitOptions.TrimEntries */);
-                
-            } catch {
-                Debug.Log(string.Format("Invalid 3 x 3 matrix supplied in command line {0}", index));
-                Debug.Log("All such matrices must be formatted like [A, B, C; D, E, F; G, H, I]");
-            }
+        } else if(Regex.Match(commands[index], "^ASSIGN-PROPERTY \"[\\w-. ]+\" [A-Z]+ ((\\[([0-9]+(.[0-9]+)?)((;|,)([0-9]+(.[0-9]+)?))*\\])|(\"[\\w-.]+\"))$").Success) {
+            Debug.Log("TODO");
         } else {
             Debug.Log(string.Format("No command, or invalid command, received at command line {0}.", index));
             Execute(index + 1);
@@ -122,9 +139,13 @@ public class LessonReader : MonoBehaviour
     }
 
     // Not quite sure what to use this for yet
-    public void SetValueAndExecuteFromSavePoint(string key, bool value) {
-        flags[key] = value;
-        Execute(savePoint);
+    public void ExecuteFromSavePoint(string password) {
+        if(password.Equals(savePhrase)) {
+            Debug.Log("Executing from save point.");
+            Execute(savePoint);
+        } else {
+            Debug.Log("Invalid signal.");
+        }
     }
     
     private IEnumerator Wait(float waitTime, int commandAfter) {
@@ -137,8 +158,6 @@ public class LessonReader : MonoBehaviour
 class Matrix
 {
     public float[,] values;
-    public int rows;
-    public int cols;
 
     public int getRows() {
         return values.GetLength(0);
@@ -154,42 +173,76 @@ class Matrix
             throw new System.ArithmeticException();
         }
         this.values = values;
-        rows = values.GetLength(0);
-        cols = values.GetLength(1);
+    }
+
+    public static Matrix operator -(Matrix a) {
+        float [,] result = new float[a.getRows(), a.getCols()];
+        for(int i = 0; i < a.getRows(); i++) {
+            for(int j = 0; j < a.getCols(); j++) {
+                result[i,j] = -a.values[i,j];
+            }
+        }
+        return new Matrix(result);
     }
 
     public static Matrix operator +(Matrix a, Matrix b) {
-        if(a.rows != b.rows || a.cols != b.cols) {
+        if(a.getRows() != b.getRows() || a.getCols() != b.getCols()) {
             Debug.Log("Invalid dimensions of added matrices.");
             throw new System.ArithmeticException();
         }
 
-        float[,] result = new float[a.rows, a.cols];
-        for(int i = 0; i < a.rows; i++) {
-            for(int j = 0; j < b.cols; j++) {
+        float[,] result = new float[a.getRows(), a.getCols()];
+        for(int i = 0; i < a.getRows(); i++) {
+            for(int j = 0; j < b.getCols(); j++) {
                 result[i,j] = a.values[i,j] + b.values[i,j];
             }
         }
+        return new Matrix(result);
+    }
 
+    public static Matrix operator -(Matrix a, Matrix b) {
+        if(a.getRows() != b.getRows() || a.getCols() != b.getCols()) {
+            Debug.Log("Invalid dimensions of added matrices.");
+            throw new System.ArithmeticException();
+        }
+
+        float[,] result = new float[a.getRows(), a.getCols()];
+        for(int i = 0; i < a.getRows(); i++) {
+            for(int j = 0; j < b.getCols(); j++) {
+                result[i,j] = a.values[i,j] - b.values[i,j];
+            }
+        }
         return new Matrix(result);
     }
 
     public static Matrix operator *(Matrix a, Matrix b) {
-        if(a.cols != b.rows) {
+        if(a.getCols() != b.getRows()) {
             Debug.Log("Invalid dimensions of multiplied matrices.");
             throw new System.ArithmeticException();
         }
 
-        float[,] result = new float[a.rows,b.cols];
-        for(int i = 0; i < a.rows; i++) {
-            for(int j = 0; j < b.cols; j++) {
+        float[,] result = new float[a.getRows(),b.getCols()];
+        for(int i = 0; i < a.getRows(); i++) {
+            for(int j = 0; j < b.getCols(); j++) {
                 float val = 0;
-                for(int k = 0; k < a.cols; k++) {
+                for(int k = 0; k < a.getCols(); k++) {
                     val += a.values[i,k] * b.values[k,j];
                 }
                 result[i,j] = val;
             }
         }
         return new Matrix(result);
+    }
+
+    public override string ToString() {
+        string mainString = "[";
+        for(int i = 0; i < this.getRows(); i++) {
+            for(int j = 0; j < this.getCols(); j++) {
+                mainString += values[i,j].ToString() + (j != this.getCols() - 1 ? "," : "");
+            }
+            mainString += (i != this.getRows() - 1 ? ";" : "");
+        }
+        mainString += "]";
+        return mainString;
     }
 }
