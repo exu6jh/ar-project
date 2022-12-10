6e46c21da73097d0262eb11950d2eeed647e2632
Everett X <exu6@jh.edu>
2022-12-09 23:57:49 -0500

    Expanded reader project script functionality

--- ReaderProject/Assets/Scripts/LessonReader.cs
+++ ReaderProject/Assets/Scripts/LessonReader.cs
@@ -58,30 +58,18 @@ public class LessonReader : MonoBehaviour
             }
 
             Execute(index + 1);
-        } else if (Regex.Match(commands[index], "^CREATE-MATRIX \"[\\w\\- ]+\" \\[(([0-9]+(.[0-9]+)?)(,[0-9]+(.[0-9]+)?)*)(;([0-9]+(.[0-9]+)?)(,[0-9]+(.[0-9]+)?)*)*\\]$").Success) {
+            return;
+        } else if (Regex.Match(commands[index], "^CREATE-MATRIX \"[\\w\\- ]+\" \\[((-?[0-9]+(.[0-9]+)?)(,-?[0-9]+(.[0-9]+)?)*)(;(-?[0-9]+(.[0-9]+)?)(,-?[0-9]+(.[0-9]+)?)*)*\\]$").Success) {
+            string matrixElements = commands[index].Split(new char[] {'[', ']'})[1];
+            string[] names = commands[index].Split("\"");
             try {
-                string matrixElements = commands[index].Split(new char[] {'[', ']'})[1];
-                string[] rows = matrixElements.Split(';');
-                string[] referenceCols = rows[0].Split(",");
-                float[,] values = new float[rows.Length, referenceCols.Length];
-                for(int i = 0; i < rows.Length; i++) {
-                    string[] cols = rows[i].Split(",");
-                    if(cols.Length != referenceCols.Length) {
-                        Debug.Log("Length mismatch.");
-                        throw new System.ArithmeticException();
-                    }
-                    for(int j = 0; j < cols.Length; j++) {
-                        values[i,j] = float.Parse(cols[j]);
-                    }
-                }
-                string[] names = commands[index].Split("\"");
-                matrices[names[1]] = new Matrix(values);
+                matrices[names[1]] = new Matrix(matrixElements);
             } catch {
-                Debug.Log(string.Format("Invalid 3 x 3 matrix supplied in command line {0}", index));
-                Debug.Log("All such matrices must be formatted like [A, B, C; D, E, F; G, H, I]");
+                Debug.Log("Error: inconsistent matrix size.");
             }
 
             Execute(index + 1);
+            return;
         } else if(Regex.Match(commands[index], "^DELETE-OBJECT \"[\\w\\-. ]+\"$").Success) {
             Debug.Log("Deleting.");
             string[] names = commands[index].Split("\"");
@@ -92,22 +80,21 @@ public class LessonReader : MonoBehaviour
             }
 
             Execute(index + 1);
+            return;
         } else if(Regex.Match(commands[index], "^WAIT [0-9]+(.[0-9]+)?$").Success) {
             Debug.Log("Waiting.");
-            try {
-                string[] scriptWords = commands[index].Split(" ");
-                float waitTime = float.Parse(scriptWords[1]);
-                Debug.Log("Starting wait.");
-                IEnumerator waitCoroutine = Wait(waitTime, index + 1);
-                StartCoroutine(waitCoroutine);
-            } catch {
-                Debug.Log(string.Format("Problem parsing wait time in command line {0}.", index));
-            }
+            string[] scriptWords = commands[index].Split(" ");
+            float waitTime = float.Parse(scriptWords[1]);
+            Debug.Log("Starting wait.");
+            IEnumerator waitCoroutine = Wait(waitTime, index + 1);
+            StartCoroutine(waitCoroutine);
+            return;
         } else if(Regex.Match(commands[index], "^WAIT-UNTIL \"[\\w\\- ]+\"$").Success) {
             Debug.Log("WAIT-UNTIL");
             string[] names = commands[index].Split("\"");
             savePhrase = names[1];
             savePoint = index + 1;
+            return;
         } else if(Regex.Match(commands[index], "^GOTO [0-9]+( IF \"[\\w-]+\" ELSE [0-9]+)?$").Success) {
             Debug.Log("GOTO");
             string[] scriptWords = commands[index].Split(" ");
@@ -130,8 +117,82 @@ public class LessonReader : MonoBehaviour
                 Debug.Log(string.Format("Invalid goto line supplied in command line {0}", index));
                 Debug.Log("All goto commands must be formatted like \"GOTO <int>\". For example, \"GOTO 10\".");
             }
-        } else if(Regex.Match(commands[index], "^ASSIGN-PROPERTY \"[\\w-. ]+\" [A-Z]+ ((\\[([0-9]+(.[0-9]+)?)((;|,)([0-9]+(.[0-9]+)?))*\\])|(\"[\\w-.]+\"))$").Success) {
-            Debug.Log("TODO");
+            return;
+        } else if(Regex.Match(commands[index], "^ASSIGN-PROPERTY \"[\\w-. ]+\" \"[A-Z]+\" ((\\[(-?[0-9]+(.[0-9]+)?)((;|,)(-?[0-9]+(.[0-9]+)?))*\\])|(\"[\\w-.]+\"))$").Success) {
+            string[] names = commands[index].Split("\"");
+            GameObject obj;
+            if(gameObjects.ContainsKey(names[1])) {
+                obj = gameObjects[names[1]];
+            } else {
+                Debug.Log(string.Format("No such object {0} detected.", names[1]));
+                Execute(index + 1);
+                return;
+            }
+
+            if(names[3].Equals("POS")) {
+                try {
+                    string matrixElements = commands[index].Split(new char[] {'[', ']'})[1];
+                    Matrix positionMatrix = new Matrix(matrixElements);
+                    if(positionMatrix.getRows() != 1 || positionMatrix.getCols() != 3) {
+                        Debug.Log("Error: position should be 1 row by 3 columns.");
+                        throw new System.ArithmeticException();
+                    }
+                    float[,] pos = positionMatrix.values;
+                    obj.transform.position = new Vector3(pos[0,0], pos[0,1], pos[0,2]);
+                } catch {
+                    Debug.Log("Error: inconsistent matrix size.");
+                }
+            } else if(names[3].Equals("ROT")) {
+                try {
+                    string matrixElements = commands[index].Split(new char[] {'[', ']'})[1];
+                    Matrix rotationMatrix = new Matrix(matrixElements);
+                    if(rotationMatrix.getRows() != 1 || rotationMatrix.getCols() != 3) {
+                        Debug.Log("Error: rotation should be 1 row by 3 columns.");
+                        throw new System.ArithmeticException();
+                    }
+                    float[,] rot = rotationMatrix.values;
+                    obj.transform.eulerAngles = new Vector3(rot[0,0], rot[0,1], rot[0,2]);
+                } catch {
+                    Debug.Log("Error: inconsistent matrix size.");
+                }
+            } else if(names[3].Equals("SCALE")) {
+                try {
+                    string matrixElements = commands[index].Split(new char[] {'[', ']'})[1];
+                    Matrix scaleMatrix = new Matrix(matrixElements);
+                    if(scaleMatrix.getRows() != 1 || scaleMatrix.getCols() != 3) {
+                        Debug.Log("Error: scale should be 1 row by 3 columns.");
+                        throw new System.ArithmeticException();
+                    }
+                    float[,] scale = scaleMatrix.values;
+                    obj.transform.localScale = new Vector3(scale[0,0], scale[0,1], scale[0,2]);
+                } catch {
+                    Debug.Log("Error: inconsistent matrix size.");
+                }
+            } else {
+                Debug.Log(string.Format("Error: no valid property {0}", names[3]));
+            }
+            Execute(index + 1);
+            return;
+        } else if(Regex.Match(commands[index], "APPLY-MATRIX \"[\\w\\- ]+\" \"[\\w\\-. ]+\"").Success) {
+            string[] names = commands[index].Split("\"");
+            if(matrices.ContainsKey(names[1]) && gameObjects.ContainsKey(names[3])) {
+                foreach(MeshFilter filters in gameObjects[names[3]].GetComponentsInChildren<MeshFilter>()) {
+                    Mesh mesh = filters.mesh;
+                    Vector3[] vertices = mesh.vertices;
+                    for(int i = 0; i < vertices.Length; i++) {
+                        float[,] vertexPosition = new float[3,1]{{vertices[i].x},{vertices[i].y},{vertices[i].z}};
+                        Matrix vertexMatrix = new Matrix(vertexPosition);
+                        Matrix transformedMatrix = matrices[names[1]] * vertexMatrix;
+                        Vector3 transformedPosition = new Vector3(transformedMatrix.values[0,0], transformedMatrix.values[1,0], transformedMatrix.values[2,0]);
+                        vertices[i] = transformedPosition;
+                    }
+                    mesh.vertices = vertices;
+                    mesh.RecalculateNormals();
+                }
+            } else {
+                Debug.Log(string.Format("Either no matrix {0} or no object {1} detected.", names[1], names[3]));
+            }
+            Execute(index + 1);
         } else {
             Debug.Log(string.Format("No command, or invalid command, received at command line {0}.", index));
             Execute(index + 1);
@@ -171,6 +232,23 @@ class Matrix
         if(values.Rank != 2) {
             Debug.Log("Invalid size of multidimensional array; array must be 2-dimensional.");
             throw new System.ArithmeticException();
+        }
+        this.values = values;
+    }
+
+    public Matrix(string input) {
+        string[] rows = input.Split(';');
+        string[] referenceCols = rows[0].Split(",");
+        float[,] values = new float[rows.Length, referenceCols.Length];
+        for(int i = 0; i < rows.Length; i++) {
+            string[] cols = rows[i].Split(",");
+            if(cols.Length != referenceCols.Length) {
+                Debug.Log("Matrix size mismatch.");
+                throw new System.ArithmeticException();
+            }
+            for(int j = 0; j < cols.Length; j++) {
+                values[i,j] = float.Parse(cols[j]);
+            }
         }
         this.values = values;
     }
